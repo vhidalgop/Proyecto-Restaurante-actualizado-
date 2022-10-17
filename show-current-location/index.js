abe59d2299bef1f6bceeb8194b19db082a2b48bc
flopox <l.llop@alumno.etec.um.edu.ar>
2022-10-17 09:47:37 -0300

    Actualizacion ubicacion actual proyecto

--- show-current-location/index.js
+++ show-current-location/index.js
@@ -1,30 +1,51 @@
-function initMap(){
-    const argCoords = {lat: -34.0, lng: -64.0}
-    const map = new google.maps.Map(mapDiv, {
-        center: argCoords,
-        zoom: 6,
-    });
-    const marker = new google.maps.Marker({
-        position: argCoords,
-        map,
-    });
+let map, infoWindow;
 
-    GamepadButton.addEventListener('click', () =>{
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(
-               ({coords: {latitude, longitude}}) =>{
-                   const coords = {
-                       lat: latitude,
-                       lng: longitude,
-                   };
-                   map.setCenter(coords)
-                   map.setZoom(8)
-                   marker.setPosition(coords)
-            }, () =>{
-               alert("El navegador esta bien, pero ocurrio un error")}
-            );
-           }else{
-               alert("Tu navegador no consta de geolocalizacion, actualizalo para continuar");
-           } 
-    });
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
