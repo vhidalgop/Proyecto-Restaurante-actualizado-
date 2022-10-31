26cf60774ca830659910c2002ef68732e0c7ed46
flopox <l.llop@alumno.etec.um.edu.ar>
2022-10-31 10:22:53 -0300

    viaje distancia

---
+++ si/Main.js
@@ -0,0 +1,56 @@
+﻿var myLatLng = { lat: -32.9, lng: -68.8 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+};
+
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+var directionsService = new google.maps.DirectionsService();
+
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+directionsDisplay.setMap(map);
+
+function calcRoute() {
+
+    var request = {
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+        travelMode: google.maps.TravelMode.DRIVING, 
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            const output = document.querySelector('#output');
+            output.innerHTML = "<div class='alert-info'>from: " + document.getElementById("from").value + ".<br />to: " + document.getElementById("to").value + ".<br /> Distance de manejo <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duracion <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+
+            directionsDisplay.setDirections(result);
+        } else {
+
+            directionsDisplay.setDirections({ routes: [] });
+
+            map.setCenter(myLatLng);
+
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> No se pudo calcular la distancia del viaje</div>";
+        }
+    });
+
+}
+
+
+
+//create autocomplete objects for all inputs
+var options = {
+    types: ['(cities)']
+}
+
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
